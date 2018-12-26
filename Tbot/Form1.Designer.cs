8d2ee7c9b0de3dd500291c5ffe89f1813aa7bbaa
ZIBER-T <ziber-t@mail.ru>
2018-12-26 16:38:38 +0300

    first push

---
+++ Tbot/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Tbot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BtnRunClick);
+            // 
+            // textKey
+            // 
+            this.textKey.Location = new System.Drawing.Point(12, 13);
+            this.textKey.Name = "textKey";
+            this.textKey.Size = new System.Drawing.Size(324, 20);
+            this.textKey.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 96);
+            this.Controls.Add(this.textKey);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.BtnRunClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textKey;
+    }
+}
+
---
+++ Tbot/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Runtime.Remoting.Channels;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tbot
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker bw;
+        public Form1()
+        {
+            InitializeComponent();
+            this.bw = new BackgroundWorker();
+            this.bw.DoWork += this.bw_DoWork;
+            textKey.Text = "713280704:AAEwVpLB0TpcLQHYSyaVx0vGDzC456gmhmE";
+        }
+
+        async void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+            var key = e.Argument as String;
+            try
+            {
+                var Bot = new Telegram.Bot.TelegramBotClient(key);
+                Console.WriteLine(key);
+                await Bot.SetWebhookAsync("");
+                int offset = 0;
+                while (true)
+                {
+                    var updates = await Bot.GetUpdatesAsync(offset);
+                    foreach (var update in updates)
+                    {
+                        var message = update.Message;
+                        if (message != null && message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+                        {
+                            //if (message.Text == "/say")
+                            //{
+                            //    await Bot.SendTextMessageAsync(message.Chat.Id, "Здарова",
+                            //        replyToMessageId: message.MessageId);
+                            //}
+
+                            switch (message.Text)
+                            {
+                                case "/say":
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "Здарова",
+                                        replyToMessageId: message.MessageId);
+                                    break;
+                                case "/ты работаешь?":
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "Да, блять! Че ты хочешь?",
+                                        replyToMessageId: message.MessageId);
+                                    break;
+                                case "/Ты работаешь?":
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "Да, блять! Че ты хочешь?",
+                                        replyToMessageId: message.MessageId);
+                                    break;
+                                case "/ты работаешь":
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "Да, блять! Че ты хочешь?",
+                                        replyToMessageId: message.MessageId);
+                                    break;
+                                case "/gena":
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "Гена - тыжпрограммист",
+                                        replyToMessageId: message.MessageId);
+                                    break;
+                                case "Фристайло":
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "Ракамакафо!",
+                                        replyToMessageId: message.MessageId);
+                                    break;
+                                case "/misha":
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "Моя броня 99 миллиметров! Что хочу ,то ворочу!",
+                                        replyToMessageId: message.MessageId);
+                                    break;
+                                case "/help":
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "/say \n /gena \n /rost \n /kat \n /ты работаешь?\n /misha \n /user \n",
+                                        replyToMessageId: message.MessageId);
+                                    break;
+                                case "/user":
+                                    //await Bot.SendTextMessageAsync(message.Chat.Id, message.From.FirstName,
+                                    //    replyToMessageId: message.MessageId);
+                                    if (message.From.FirstName != null )
+                                    {
+                                        await Bot.SendTextMessageAsync(message.Chat.Id, message.From.FirstName+"\n"+message.From.Id+"\n"+message.From.LastName+"\n",
+                                            replyToMessageId: message.MessageId);
+                                    }
+                                   
+                                    break;
+                                case "/rost":
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "Медузаааа! Кутузав - без глАза!",
+                                        replyToMessageId: message.MessageId);
+                                    break;
+                                case "/nik":
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, " Так Promise или нет?)",
+                                        replyToMessageId: message.MessageId);
+                                    break;
+                                case "/kat":
+                                    await Bot.SendTextMessageAsync(message.Chat.Id, "Катя супер СЕНЬЕРИТА! А PHP говно!)",
+                                        replyToMessageId: message.MessageId);
+                                    break;
+                                default:
+                                    //await Bot.SendTextMessageAsync(message.Chat.Id, "Что то пошло не так...",
+                                    //    replyToMessageId: message.MessageId);
+                                    break;
+                            }
+                        }
+                        //Console.WriteLine(update.Type);
+                        offset = update.Id + 1;
+                    }
+                }
+            }
+            catch (Telegram.Bot.Exceptions.ApiRequestException ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        void BtnRunClick(object sender, EventArgs e)
+        {
+            var text = textKey.Text;
+            if (this.bw.IsBusy != true)
+            {
+                this.bw.RunWorkerAsync(text);
+                MessageBox.Show("Run");
+            }
+        }
+    }
+}
